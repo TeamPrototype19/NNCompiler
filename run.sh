c8912bbaade333c656017da22fea83c47003dfa1
Deokhwan,Kim <thekwan@naver.com>
2019-08-14 01:09:28 +0900

    Add test-set (LeNet)

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-./nnc -g testsets/deploy_inception-v3.prototxt -o output.dot
-dot -Tpng output.dot -o output.png
+echo "========= Inception V3 network =========="
+#./nnc -g testsets/deploy_inception-v3.prototxt -o output.dot
+#dot -Tpng inceptionv1.dot -o inceptionv1.png
+echo "========= Lenet network        =========="
+./nnc -g testsets/lenet.prototxt -o lenet.dot
+dot -Tpng lenet.dot -o lenet.png
